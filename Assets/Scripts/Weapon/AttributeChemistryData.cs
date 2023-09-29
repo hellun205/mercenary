ab29c95962beba7a9eca9a48429456703ce5808b
yellow sky <hellun515@gmail.com>
2023-09-29 14:46:05 +0900

    attribute data

--- Assets/Scripts/Weapon/AttributeChemistryData.cs
+++ Assets/Scripts/Weapon/AttributeChemistryData.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Data;
 using UnityEngine;
 
 namespace Weapon
 {
   [Serializable]
-  public class AttributeChemistryData
+  public class AttributeChemistryData : IData<Dictionary<Attribute, Dictionary<int, Dictionary<ApplyStatus, float>>>>
   {
     [Serializable]
     public class AttributeItem
@@ -19,16 +21,31 @@ namespace Weapon
           public ApplyStatus type;
           public float value;
         }
-        
+
         public int count;
         public ApplyItem[] apply;
       }
-      
+
       public Attribute type;
       public StatusItem[] status;
     }
-    
+
     // public Dictionary<Attribute, Dictionary<string, Dictionary<ApplyStatus, float>>> data;
     public AttributeItem[] data;
+
+    public Dictionary<Attribute, Dictionary<int, Dictionary<ApplyStatus, float>>> ToSimply()
+      => data.ToDictionary
+      (
+        ai => ai.type,
+        ai => ai.status.ToDictionary
+        (
+          si => si.count,
+          si => si.apply.ToDictionary
+          (
+            api => api.type,
+            api => api.value
+          )
+        )
+      );
   }
 }
